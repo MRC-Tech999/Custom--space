ca2e7e0c759bc1d99ea8f85ac7b5c7401ee84e5f
MRC-Tech999 <emprerorsukuna@gmail.com>
2025-04-17 15:46:21 -0400

    Update user.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true, match: /^[a-z0-9]+$/ },
   password: { type: String, required: true },
-  username: { type: String, required: true, unique: true },
-  followers: [String],
-  certified: Boolean,
+  profilePicture: { type: String, default: '' },
+  bio: { type: String, default: '' },
+  isCertified: { type: Boolean, default: false },
+  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  notifications: [{ type: String }]
 });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model('User', UserSchema);
